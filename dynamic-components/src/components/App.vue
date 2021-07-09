dc4a354f461f86e73638a6fb3fe12dbc2d7969b3
Axel Wulff Sæther <axelwulff@mac.com>
2021-07-09 11:19:16 +0200

    dynamic-components

---
+++ dynamic-components/src/components/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <select v-model="componentName">
+      <option value="Home">Home</option>
+      <option value="About">About</option>
+    </select>
+    <keep-alive>
+      <component :is="componentName"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import About from './About.vue';
+import Home from './Home.vue';
+export default {
+  name: 'App',
+  components: {About, Home},
+  data() {
+    return {
+      componentName: "Home"
+    }
+  }
+};
+</script>
