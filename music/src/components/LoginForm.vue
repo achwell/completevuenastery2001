97c59f5cdbc260136cb6156e072a23fff9023960
Axel Wulff Sæther <axelwulff@mac.com>
2021-07-09 19:44:50 +0200

    Etter Section 10

---
+++ music/src/components/LoginForm.vue
@@ -0,0 +1,64 @@
+<template>
+  <div
+    class="text-white text-center font-bold p-4 mb4"
+    v-if="login_show_alert" :class="login_alert_variant">
+    {{login_alert_msg}}
+  </div>
+  <VeeForm :validation-schema="loginSchema" @submit="logIn">
+    <!-- Email -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Email</label>
+      <VeeField type="email" name="email"
+                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
+                  duration-500 focus:outline-none focus:border-black rounded"
+                placeholder="Enter Email" />
+      <ErrorMessage class="text-red-600" name="email" />
+    </div>
+    <!-- Password -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Password</label>
+      <VeeField name="password" :bails="false" v-slot="{ field, errors }">
+        <input class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300
+                 transition duration-500 focus:outline-none focus:border-black rounded"
+               type="password" placeholder="Password" v-bind="field"/>
+        <div class="text-red-600" v-for="error in errors" :key="error">{{ error }}</div>
+      </VeeField>
+    </div>
+    <button type="submit"
+            class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
+                hover:bg-purple-700" :disabled="login_in_submission">
+      Submit
+    </button>
+  </VeeForm>
+
+</template>
+<script>
+
+export default {
+  name: 'LoginForm',
+  data() {
+    return {
+      login_in_submission: false,
+      login_show_alert: false,
+      login_alert_variant: 'bg-blue-500',
+      login_alert_msg: 'Please wait! Your are being logged in.',
+      loginSchema: {
+        email: 'required|email',
+        password: 'required|min:3|max:100',
+      },
+    };
+  },
+  methods: {
+    logIn(values) {
+      this.login_in_submission = true;
+      this.login_show_alert = true;
+      this.login_alert_variant = 'bg-blue-500';
+      this.login_alert_msg = 'Please wait! We are logging you in.';
+
+      this.login_alert_variant = 'bg-green-500';
+      this.login_alert_msg = 'Success! Your are now logged in.';
+      console.log(values);
+    },
+  },
+};
+</script>
--- music/src/includes/validation.js
+++ music/src/includes/validation.js
@@ -1,9 +1,20 @@
 import {
-  Form as VeeForm, Field as VeeField, defineRule, ErrorMessage,
+  configure,
+  defineRule,
+  ErrorMessage,
+  Field as VeeField,
+  Form as VeeForm,
 } from 'vee-validate';
 import {
-  required, min, max, alpha_spaces as alphaSpaces, email, min_value as minVal, max_value as maxVal,
-  confirmed, not_one_of as excluded,
+  alpha_spaces as alphaSpaces,
+  confirmed,
+  email,
+  max,
+  max_value as maxVal,
+  min,
+  min_value as minVal,
+  not_one_of as excluded,
+  required,
 } from '@vee-validate/rules';
 
 export default {
@@ -11,6 +22,7 @@ export default {
     app.component('VeeForm', VeeForm);
     app.component('VeeField', VeeField);
     app.component('ErrorMessage', ErrorMessage);
+
     defineRule('required', required);
     defineRule('min', min);
     defineRule('max', max);
@@ -18,7 +30,35 @@ export default {
     defineRule('email', email);
     defineRule('min_value', minVal);
     defineRule('max_value', maxVal);
-    defineRule('confirmed', confirmed);
+    defineRule('password_mismatch', confirmed);
     defineRule('excluded', excluded);
+    defineRule('country_excluded', excluded);
+    defineRule('tos', required);
+
+    configure({
+      generateMessage: ({ rule, field }) => {
+        const param = rule.params ? rule.params[0] : '';
+        const messages = {
+          required: `The field ${field} is required.`,
+          min: `The field ${field} must be at least ${param} characters.`,
+          max: `The field ${field} can not be more than ${param} characters.`,
+          alpha_spaces: `The field ${field} may only contail alphabetic characters and spaces.`,
+          email: `The field ${field} must be a valid email.`,
+          min_value: `The field ${field} must be higher than or equal to ${param}.`,
+          max_value: `The field ${field} must be lower than or equal to ${param}.`,
+          password_mismatch: 'The passwords does not match.',
+          excluded: `You are not allowed to use this value for the field ${field}.`,
+          country_excluded: 'Due to restrictions, we do not accept users from this location.',
+          tos: 'You must accept the Terms of Service.',
+        };
+        return messages[rule.name]
+          ? messages[rule.name]
+          : `The field ${field} is invalid.`;
+      },
+      validateOnBlur: true,
+      validateOnChange: true,
+      validateOnInput: false,
+      validateOnModelUpdate: true,
+    });
   },
 };
