fd9f2a1cae585ed3e4e71c0dffd3064feab1208a
Axel Wulff Sæther <axelwulff@mac.com>
2021-07-09 11:47:53 +0200

    slots

---
+++ slots/src/components/Form.vue
@@ -0,0 +1,14 @@
+<template>
+  <form>
+      <div class="help">
+        <slot name="help">This is some help text.</slot>
+      </div>
+      <div class="fields">
+        <slot name="fields">This is some fields.</slot>
+      </div>
+      <div class="buttons">
+        <slot name="buttons">This is some buttons.</slot>
+      </div>
+      <slot></slot>
+  </form>
+</template>
